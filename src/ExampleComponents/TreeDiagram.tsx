fb446af9043f02fba4df55483ce7851963d28dad
Sophia Zheng <sjzheng@mit.edu>
2024-02-26 17:05:07 -0500

    working on collapsed function

--- src/ExampleComponents/TreeDiagram.tsx
+++ src/ExampleComponents/TreeDiagram.tsx
@@ -1,22 +1,32 @@
 import { HierarchyNode } from "d3-hierarchy";
-import { For, Show } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 
 // counter to keep track of each rectangle rendered
+// 1. If you do counter + 1 instead of counter++;
+// 2.
+
 let counter = 0;
 
 const TreeDiagram = (props: any) => {
   console.log("props", props);
 
   const { hierarchy } = props;
-  
+
+  // for each node have a state for "collapsed/not"
+  // mutate that state for the on click
+  // if node is collapsed, do not render any children elements
+  // when click is first made, also set counter to 0
+
   if (!hierarchy) {
     return <div></div>;
   }
 
   console.log(hierarchy);
 
+  const [collapsedStore, setCollapsedStore] = createSignal({});
+
   console.log("hie", hierarchy());
-  
+
   return (
     <svg width={1000} height={8000}>
       <For each={hierarchy()?.children}>
@@ -25,6 +35,8 @@ const TreeDiagram = (props: any) => {
             // begin recursion to display elements
             <NodeElementRecursive
               node={node}
+              collapsedStore={collapsedStore()}
+              setCollapsedStore={setCollapsedStore}
               i={i}
               level={0}
               counter={counter}
@@ -38,12 +50,32 @@ const TreeDiagram = (props: any) => {
 
 // functional component -> recurse through every child of the current node
 const NodeElementRecursive = (props: any) => {
-  let { node, i, level } = props;
+  const nodeId = props.node.data.id;
 
+  let { node, i, level, collapsedStore, setCollapsedStore } = props;
+  // Function to toggle collapsed state and reset counter
+  const toggleCollapse = () => {
+    console.log("clicked!", props);
+    counter = 0; // Reset counter on first click
+    // copies the collapsedStore
+    const clone = Object.assign({}, collapsedStore());
+    clone[nodeId] = !clone[nodeId];
+
+    console.log("clone", clone);
+    setCollapsedStore(clone);
+  };
+
+  console.log("collapsed", collapsedStore);
   return (
     <g>
-      <NodeElement {...props} counter={counter++}/>
-      <Show when={node.children} fallback={<g></g>}>
+      <g onClick={() => toggleCollapse()}>
+        <NodeElement {...props} counter={counter++} />
+      </g>
+
+      <Show
+        when={node.children && !collapsedStore()?.[nodeId]}
+        fallback={<g></g>}
+      >
         <For each={node.children}>
           {(childNode, i) => {
             return (
@@ -64,7 +96,7 @@ const NodeElementRecursive = (props: any) => {
 // presentational component -> generate the rectangles based on its properties
 const NodeElement = (props: any) => {
   const { node, i, level } = props;
-//   console.log('visitng node',node.data.name,'counter',counter,'i',i)
+  //   console.log('visitng node',node.data.name,'counter',counter,'i',i)
 
   const levelOffset = level * 30;
   const xPosition = 20 + levelOffset;
